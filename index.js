6bc22fc9c258d0a982a00b0f9959e78fa2b378a9
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-06 16:56:13 +0600

    add html and javascript for project

---
+++ index.js
@@ -0,0 +1,15 @@
+var s=parseFloat(prompt("Enter first number : "));
+var m=parseFloat(prompt("Enter last number : "));
+sum=0;
+for(var x=s;x<=m;x=x+1){
+          sum=sum+x;
+}
+document.write("Result = " + sum);
+
+
+
+
+
+
+
+
